4819985f1ce130f321295ca58b607ed76e49f2a2
awryme <awryme@gmail.com>
2017-09-19 13:29:20 +0300

    first commit

---
+++ app/src/main/java/com/example/andreyko0/myapplication/ScrollingActivity.java
@@ -0,0 +1,23 @@
+package com.example.andreyko0.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    public void doClick(View v) {
+        startActivity(new Intent(this, ProductActivity.class));
+    }
+}
