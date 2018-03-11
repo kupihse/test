a9e75b78cd139730a153f3f35359639aee465b66
kupihse <kupihse@gmail.com>
2018-03-11 11:50:00 +0300

    всякие мелочи
    добавил клик по логину для отправки почты
    убрал кнопку отправить мэил
    убрал отображение пароля у юзера
    на странице юзера в тулбаре вместо почты поставил имя
    
    начал делать настройки - не получилось, поэтому пусто

--- app/src/main/java/com/example/fragments/UserPageFragment.java
+++ app/src/main/java/com/example/fragments/UserPageFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -70,6 +71,20 @@ public class UserPageFragment extends Fragment {
             }
         });
 
+
+
+        final TextView login = (TextView) rootView.findViewById(R.id.user_page_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", String.valueOf(login.getText().subSequence(8, login.getText().length())),
+                        null));
+                startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+            }
+        });
+
+
         Services.users.get(CurrentUser.getLogin()).enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
@@ -109,28 +124,8 @@ public class UserPageFragment extends Fragment {
 //        Button myProducts = (Button) root.findViewById(R.id.user_page_products);
 //        myProducts.setVisibility(View.VISIBLE);
 
-        final Button sendMail = (Button) root.findViewById(R.id.user_page_sendmail);
-        sendMail.setVisibility(View.INVISIBLE);
-
-        sendMail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendMail(root);
-            }
-        });
-
     }
 
-    public void sendMail(View v) {
-        TextView login = (TextView) v.findViewById(R.id.user_page_login);
-        Toast.makeText(getContext(), "pressed", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                "mailto", String.valueOf(login.getText().subSequence(8, login.getText().length())),
-                null));
-        startActivity(Intent.createChooser(intent, "Choose an Email client :"));
-//        Toast.makeText(UserPageActivity.this, login.getText().subSequence(8,login.getText().length()),
-//                Toast.LENGTH_SHORT).show();
-    }
 
     public void myProductsButton(View v) {
         for (String p : user.getProducts()) {
